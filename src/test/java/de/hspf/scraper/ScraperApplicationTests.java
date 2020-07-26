a6fb3b180756e7fd25cc80482953877ae105293e
Marcel <viehmaie@hs-pforzheim.de>
2020-07-26 21:25:20 +0200

    first commit

---
+++ src/test/java/de/hspf/scraper/ScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hspf.scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
